d00aa5ce965de4252b7e13885559d114f18faa32
unknown <michael.hunt@CAM-7WTC6X1.cornwall.ac.uk>
2015-06-24 14:37:59 +0100

    ModellingExpert done

--- Unit3/Unit3_ModelingExpert.R
+++ Unit3/Unit3_ModelingExpert.R
@@ -4,7 +4,7 @@
 # Video 4
 
 # Read in dataset
-quality = read.csv("quality.csv")
+quality = read.csv("./data/quality.csv")
 
 # Look at structure
 str(quality)
@@ -16,7 +16,7 @@ table(quality$PoorCare)
 98/131
 
 # Install and load caTools package
-install.packages("caTools")
+#install.packages("caTools")
 library(caTools)
 
 # Randomly split data
@@ -40,6 +40,15 @@ summary(predictTrain)
 tapply(predictTrain, qualityTrain$PoorCare, mean)
 
 
+# QQ
+#QualityLog = glm(PoorCare ~ StartedOnCombination + ProviderCount, data=qualityTrain, family=binomial)
+#summary(QualityLog)
+
+#QQ
+library(ROCR)
+predictTest = predict(QualityLog, type="response", newdata=qualityTest)
+ROCRpredTest = prediction(predictTest, qualityTest$PoorCare)
+auc = as.numeric(performance(ROCRpredTest, "auc")@y.values)
 
 # Video 5
 
@@ -69,7 +78,7 @@ table(qualityTrain$PoorCare, predictTrain > 0.2)
 # Video 6
 
 # Install and load ROCR package
-install.packages("ROCR")
+#install.packages("ROCR")
 library(ROCR)
 
 # Prediction function
@@ -86,3 +95,35 @@ plot(ROCRperf, colorize=TRUE)
 
 # Add threshold labels 
 plot(ROCRperf, colorize=TRUE, print.cutoffs.at=seq(0,1,by=0.1), text.adj=c(-0.2,1.7))
+
+#qq
+# Randomly split data
+set.seed(88)
+split = sample.split(quality$PoorCare, SplitRatio = 0.75)
+split
+
+# Create training and testing sets
+qualityTrain = subset(quality, split == TRUE)
+qualityTest = subset(quality, split == FALSE)
+
+# Logistic Regression Model
+QualityLog = glm(PoorCare ~ OfficeVisits + Narcotics, data=qualityTrain, family=binomial)
+summary(QualityLog)
+
+# Make predictions on training set
+predictTrain = predict(QualityLog, type="response")
+
+# Analyze predictions
+summary(predictTrain)
+tapply(predictTrain, qualityTrain$PoorCare, mean)
+
+
+# QQ
+#QualityLog = glm(PoorCare ~ StartedOnCombination + ProviderCount, data=qualityTrain, family=binomial)
+#summary(QualityLog)
+
+#QQ
+library(ROCR)
+predictTest = predict(QualityLog, type="response", newdata=qualityTest)
+ROCRpredTest = prediction(predictTest, qualityTest$PoorCare)
+auc = as.numeric(performance(ROCRpredTest, "auc")@y.values)
